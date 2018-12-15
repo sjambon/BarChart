68c4817ffb19b2cae8c881b27239716abd8ec57f
SJ-Desktop <steven.jambon@ugent.be>
2018-12-15 15:19:53 +0100

    Project Finished

--- src/BarChart/BarChart.java
+++ src/BarChart/BarChart.java
@@ -1,15 +1,54 @@
 
 package BarChart;
 
+/**
+ * Project Java Fundamentals: BarChart klasse die een tabel bestaande uit rijen en kolommen kan visualiseren.
+ *
+ * @author Steven Jambon
+ * @version december 2018
+ */
 
 public class BarChart {
+
+    /**
+     * In dit veld wordt de titel van de tabel opgeslagen.
+     */
     private final String TITLE;
+
+    /**
+     * In dit veld wordt het maximum aantal groepen opgeslagen.
+     */
     private final int NRGROUPS;
+
+    /**
+     * In dit veld worden alle rijen (categorieën) bijgehouden in een array van Strings.
+     */
     private String[] categories;
+
+    /**
+     * In dit veld wordt de data van alle groepen bijgehouden in een 2dim array van integer getallen.
+     */
     private int[][] data;
+
+    /**
+     * In dit veld worden de namen van alle kolommen (groepen) bijgehouden in een array van Strings.
+     */
     private String[] groups;
+
+    /**
+     * In dit veld worden alle symbolen waarmee een groep wordt aangeduid bijgehouden in een array van chars.
+     */
     private char[] symbols;
 
+    /**
+     * Constructor methode voor 1 groep.
+     *
+     * @param title      de titel van de tabel.
+     * @param categories de namen van de rijen (categorieën).
+     * @param groupName  de naam van de kolom (groep).
+     * @param symbol     het symbool van de groep.
+     * @param data       de data die in de tabel zit.
+     */
     public BarChart(String title, String[] categories, String groupName, char symbol, int[] data) {
         this.TITLE = title;
         this.NRGROUPS = 1;
@@ -19,6 +58,13 @@ public class BarChart {
         this.data = new int[][]{data};
     }
 
+    /**
+     * Constructor methode voor meerdere groepen
+     *
+     * @param title      de titel van de tabel.
+     * @param nrGroups   het aantal kolommen (groepen) er zijn.
+     * @param categories het aantal rijen (categorieën) er zijn.
+     */
     public BarChart(String title, int nrGroups, String[] categories) {
         this.TITLE = title;
         this.NRGROUPS = nrGroups;
@@ -28,6 +74,14 @@ public class BarChart {
         this.symbols = new char[nrGroups];
     }
 
+    /**
+     * Voegt data toe aan de tabel.
+     *
+     * @param groupName de naam van de kolom (groep).
+     * @param symbol    het symbool van de groep.
+     * @param data      de data die in die kolom zit.
+     * @return Booleanse waarde die zegt of de data al dan niet is toegevoegd.
+     */
     public boolean putGroupData(String groupName, char symbol, int[] data) {
         int eersteLegePlaats = 0;
         for (String item : groups) {
@@ -55,6 +109,11 @@ public class BarChart {
         return true;
     }
 
+    /**
+     * Toont de data die in de tabel zit.
+     *
+     * @return De data die in de tabel zit.
+     */
     public String showData() {
         String outputString = "";
         if (NRGROUPS == 1) {
@@ -77,6 +136,11 @@ public class BarChart {
         return outputString;
     }
 
+    /**
+     * Toont de legende van de tabel
+     *
+     * @return De legende van de tabel.
+     */
     public String showLegend() {
         String outputString = "Legend:\t";
         for (int i = 0; i < groups.length; i++) {
@@ -85,6 +149,12 @@ public class BarChart {
         return outputString;
     }
 
+    /**
+     * Bepaalt de grootste waarde die in de meegegeven tabel zit.
+     *
+     * @param tabel bevat data die in de tabel weergegeven kan worden.
+     * @return De grootste waarde van de meegegeven tabel.
+     */
     private int maxValueTable(int[][] tabel) {
         int maxVal = 0;
         for (int i = 0; i < tabel.length; i++) {
@@ -97,6 +167,13 @@ public class BarChart {
         return maxVal;
     }
 
+    /**
+     * Maakt een kolom- of staaf-diagram dat al dan niet gestapeld is naargelang de meegegeven parameters.
+     *
+     * @param orientation bepaalt of er een kolom- of staaf-diagram moet gegenereerd worden.
+     * @param stacked     bepaalt of de kolom- of staaf-diagram gestapeld moet zijn of niet.
+     * @return Een kolom- of staaf-diagram dat al dan niet gestapeld is.
+     */
     public String makeChart(char orientation, boolean stacked) {
         String outputString = "";
         outputString += "Title :\t" + TITLE + "\n\n";
@@ -131,19 +208,27 @@ public class BarChart {
                             }
                             outputString += " ";
                         }
-                        outputString += "\t\t";
+                        if (NRGROUPS == 1) {
+                            outputString += "\t\t\t\t";
+                        } else {
+                            outputString += "\t\t\t";
+                        }
                     }
                     outputString += "\n";
                 }
                 for (String categorie : categories) {
-                    outputString += categorie + "\t\t";
+                    if (categories.length == 1) {
+                        outputString += categorie + "\t\t";
+                    } else {
+                        outputString += categorie + "\t\t\t";
+                    }
                 }
                 outputString += "\n\n\n" + showLegend();
             } else {
                 System.out.println("Buiten de scope van dit project.");
             }
         } else {
-            System.out.println("ERROR: ongeldige orientatie.");
+            System.out.println("ERROR: ongeldige oriëntatie.");
         }
         return outputString;
     }
--- src/BarChart/DemoBarChart.java
+++ src/BarChart/DemoBarChart.java
@@ -1,24 +1,34 @@
 package BarChart;
 
-
+/**
+ * Project Java Fundamentals: DemoBarChart klasse die de BarChart klasse test.
+ *
+ * @author Steven Jambon
+ * @version december 2018
+ */
 public class DemoBarChart {
 
-   
+    /**
+     * Het hoofdprogramma die de BarChart klasse volledig uittest.
+     *
+     * @param args de input voor het programma (hier niet van toepassing).
+     * @see BarChart
+     */
     public static void main(String[] args) {
         // Scenario 1 : 1 groep
         BarChart chart1group = new BarChart("BarChart with only 1 group", new String[]{"Boys", "Girls", "Adults"},
                 "Blue", 'B', new int[]{12, 5, 8});
-        
+
         System.out.println("\nTesting the show methods : ");
         System.out.println(chart1group.showData());
         System.out.println(chart1group.showLegend());
-        
+
 
         // Scenario 2 : staafdiagram 1 groep
-        System.out.println(chart1group.makeChart('H',false));
+        System.out.println(chart1group.makeChart('H', false));
 
         //Scenario 3 : kolomdiagram 1 groep
-        System.out.println(chart1group.makeChart('V',false));
+        System.out.println(chart1group.makeChart('V', false));
 
         //Scenario 4 : meerdere groepen
         BarChart chart3groups = new BarChart("BarChart with 3 groups", 3, new String[]{"Boys", "Girls", "Adults"});
@@ -36,15 +46,15 @@ public class DemoBarChart {
         System.out.println(chart3groups.showLegend());
 
         // Scenario 5 gegroepeerd staafdiagram
-        System.out.println(chart3groups.makeChart('H',false));
+        System.out.println(chart3groups.makeChart('H', false));
 
         // Scenario 6 gestapeld staafdiagram
-        System.out.println(chart3groups.makeChart('H',true));
+        System.out.println(chart3groups.makeChart('H', true));
 
         // Scenario 7 gegroepeerd kolomdiagram
-        System.out.println(chart3groups.makeChart('V',false));
+        System.out.println(chart3groups.makeChart('V', false));
 
 
     }
-    
+
 }
